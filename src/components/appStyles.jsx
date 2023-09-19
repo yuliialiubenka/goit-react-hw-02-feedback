4507af3bc4de5de5aabf251e7d7a4fec64872a6f
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-09-19 17:50:38 +0300

    Feedback React.js

---
+++ src/components/appStyles.jsx
@@ -0,0 +1,10 @@
+import { createUseStyles } from "react-jss";
+
+export const useStyles = createUseStyles({
+    section: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 80,
+        paddingTop: 80,
+    }
+});
